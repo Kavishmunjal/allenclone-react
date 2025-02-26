12b240befbec48e0f4b21898326726ebb2ce79cd
Kavishmunjal <kavishmunjal2300@gmail.com>
2025-02-26 11:03:51 +0530

    Allen Clone navbar

---
+++ public/src/component/layout.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link , Outlet} from "react-router-dom";
+import Logo from "../assets/logo.png";
+import "./navStyle.css";
+
+
+function Layout() {
+ 
+
+  return <div className="navbar">
+    <Link to="/" className="logo"><img src={Logo} alt="Logo" width="90" height="35" /></Link>
+    <Link to="/Course" className="course">Course</Link>
+    <Link to="/TestSeries" className="test">Test Series</Link>
+    <Link to="/Scolarship" className="scholarship">Scholarship</Link>
+    <Link to="/Results" className="results">Results</Link>
+    <Link to="/About-us" className="about">About Us</Link>
+    
+
+
+
+  <div>
+     <Outlet /> 
+  </div>
+     
+  </div>
+}
+
+
+   
+
+export default Layout
---
+++ public/src/component/navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Layout from "./layout";
+import Home from "./Home";
+
+function Navbar() {
+  return (
+    <div>
+      <div>
+        {/* <BrowserRouter>
+             <Routes>
+                <Route path="/" element={<Layout />}/>
+               
+                  
+         </ Routes>
+        </BrowserRouter> */}
+
+
+
+
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
